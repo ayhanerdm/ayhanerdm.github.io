3d204c8dd076ecd3403a58734ca8eea0045e8809
Ayhan Erdem <ayhanerdm.code@gmail.com>
2025-04-26 03:09:09 +0300

    Test

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+const log = (message) => {
+
+    const logElement = document.getElementById("log");
+    const newElement = document.createElement("div");
+    newElement.textContent = message;
+    logElement.appendChild(newElement);
+};
+
+const scanButton = document.getElementById("scan-button");
+const writeButton = document.getElementById("write-button");
+
+scanButton.addEventListener("click", async () => {
+    log("User clicked scan button");
+  
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      log("> Scan started");
+  
+      ndef.addEventListener("readingerror", () => {
+        log("Argh! Cannot read data from the NFC tag. Try another one?");
+      });
+  
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        log(`> Serial Number: ${serialNumber}`);
+        log(`> Records: (${message.records.length})`);
+      });
+    } catch (error) {
+      log("Argh! " + error);
+    }
+  });
+  
+  writeButton.addEventListener("click", async () => {
+    log("User clicked write button");
+  
+    try {
+      const ndef = new NDEFReader();
+      await ndef.write("Hello world!");
+      log("> Message written");
+    } catch (error) {
+      log("Argh! " + error);
+    }
+  });
